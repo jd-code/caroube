58bbbff0129325bc45cfdbef488685b0608554a9
jd <jd@32c91515-bade-0310-a695-fcfe42eb2883>
2011-02-22 14:54:12 +0000

    added jack support pauseaudio to be called instead of SDL_PauseAudio, now ... leaveaudio to be called before leaving app, now ...
    git-svn-id: svn://sub.nekodune.net/subdisj/bzoums/caroube@631 32c91515-bade-0310-a695-fcfe42eb2883

--- src/audio.cpp
+++ src/audio.cpp
@@ -30,7 +30,9 @@
 #define CAROUBE_H_GLOBINST
 #include "caroube.h" 
 #include "config.h"
-
+#ifdef CAROUBCANJACK
+#include <jack/jack.h> 
+#endif
 
 namespace caroube {
 using namespace std ;
@@ -98,8 +100,76 @@ ostream& operator<< (ostream & out, Envelope & e) {
 }
     
 
+// ---------- jack stuff -----------------------------------------------------------
+#ifdef CAROUBCANJACK
+
+bool usejack = false;	    // are we currently using jack ?
+bool leftjack = false;	    // have we been laid off from some jack server ?
+
+jack_port_t *output_portl,  // jack outlets
+	    *output_portr;
+jack_client_t *jclient;	    // jack-client handle towrad the jack-server
+
+
+void jack_shutdown (void *arg) {
+    jclient = NULL;
+    leftjack = true;
+}
+#endif
+
 // ---------------------------------------------------------------------------------
 
+    // this code is duplicated within the jack call-back routine and the SDL call-back routine
+
+
+#ifdef CAROUBCANJACK
+int jackcallback (jack_nframes_t nb, void *jack_arg) {
+    unsigned int i;
+
+    jack_default_audio_sample_t *jackoutr =
+        (jack_default_audio_sample_t *) jack_port_get_buffer (output_portr, nb);
+    jack_default_audio_sample_t *jackoutl =
+        (jack_default_audio_sample_t *) jack_port_get_buffer (output_portl, nb);
+
+    PlayedPSonGe::iterator ilo;
+    Sint16 sample;
+
+//	wondering if the nb param would gitter ...
+//	static int dumpit = 0;
+//	dumpit ++;
+//	if (dumpit == 1) {
+//	    cerr << "jackcallback nb = " << nb << endl;
+//	}
+//	if (dumpit == 27)
+//	    dumpit = 0;
+
+    for (i=0 ; i<nb ; i++) {
+	if ((i & BUCKETMASK) == 0) {	    // JDJDJDJD il faudrait avoir un compteur general sinon ca peut se decaler ...
+	    for (ilo=lo.begin() ; ilo!=lo.end() ; ) {
+		isonge = ilo;
+		ilo++;				// we increment before calling bucketComp, because...
+		isonge->first->bucketComp() ;   // ...bucketComp may supress the SonGe currently pointed by isonge 
+	    }
+	}
+      
+        Sint32 Cl = 0,
+               Cr = 0;
+	for (ilo=lo.begin() ; ilo!=lo.end() ; ilo++) {
+	    sample = ilo->first->getSample();
+	    Cl += (sample * ilo->first->vl) >> 8;
+	    Cr += (sample * ilo->first->vr) >> 8;
+	}
+	// ---- clipping tests
+	if (Cl > SINT16_MAX) Cl = SINT16_MAX; else if (Cl < SINT16_MIN) Cl = SINT16_MIN;
+	if (Cr > SINT16_MAX) Cr = SINT16_MAX; else if (Cr < SINT16_MIN) Cr = SINT16_MIN;
+
+	*jackoutl ++ = ((jack_default_audio_sample_t) Cl)/32768.0;
+	*jackoutr ++ = ((jack_default_audio_sample_t) Cr)/32768.0;
+    }
+
+    return 0;
+}
+#endif
 
 void fill_audio( void *udata, Uint8 *stream, int len) {
     int i;
@@ -111,7 +181,7 @@ void fill_audio( void *udata, Uint8 *stream, int len) {
     Sint16 sample;
 
     for (i=0 ; i<nb ; i++) {
-	if ((i & BUCKETMASK) == 0) {
+	if ((i & BUCKETMASK) == 0) {	    // JDJDJDJD il faudrait avoir un compteur general sinon ca peut se decaler ...
 	    for (ilo=lo.begin() ; ilo!=lo.end() ; ) {
 		isonge = ilo;
 		ilo++;				// we increment before calling bucketComp, because...
@@ -132,7 +202,7 @@ void fill_audio( void *udata, Uint8 *stream, int len) {
 
 	*p++ = Cl;
 	*p++ = Cr;
-    } 
+    }
 }
 
 
@@ -153,8 +223,60 @@ void PlayedPSonGe::push (SonGe *p) {
 
 // ---------------------------------------------------------------------------------
 
+void pauseaudio (int pause_on) {
+#ifdef CAROUBCANJACK
+    if (usejack) {
+	if (pause_on == 0) {
+	    if (jack_activate (jclient)) {
+		cerr << "cannot activate jack as client" << endl;
+		leftjack = true;    // we must leave cleanly later since jackd's not here ...
+	    }
+	} else {
+	    if (jack_deactivate (jclient)) {
+		cerr << "cannot deactivate jack as client" << endl;
+		leftjack = true;    // we must leave cleanly later since jackd's not here ...
+	    }
+	}
+    } else {
+#endif
+    SDL_PauseAudio(pause_on);
+#ifdef CAROUBCANJACK
+    }
+#endif
+}
 
-bool initaudio(size_t nbsamples) {
+bool initaudio(string & appname, size_t nbsamples) {
+#ifdef CAROUBCANJACK
+    char *env_sdl_audiodriver = getenv ("SDL_AUDIODRIVER");
+if (env_sdl_audiodriver != NULL)
+    if ((env_sdl_audiodriver != NULL) && (strncmp (env_sdl_audiodriver, "jack", 4)) == 0) {
+	usejack = true;
+    }
+
+    if (usejack) {
+	jack_status_t status;
+	jclient = jack_client_open (appname.c_str(), JackNoStartServer, &status);
+	if (jclient == NULL) {
+	    if (status & JackServerFailed) {
+		cerr << "could not connect to jackd server, not running ?" << endl;
+	    } else {
+		cerr << "could not connect to jackd server, status :" << status << endl;
+	    }
+	    return false;
+	}
+
+	if (status & JackNameNotUnique) {
+	    appname = jack_get_client_name (jclient);
+	}
+
+	jack_set_process_callback (jclient, jackcallback, 0);
+	jack_on_shutdown (jclient, jack_shutdown, 0);
+	cerr << "engine sample rate: " << jack_get_sample_rate (jclient) << endl;
+
+	output_portl = jack_port_register (jclient, "outputl", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+	output_portr = jack_port_register (jclient, "outputr", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+    } else {
+#endif
     SDL_AudioSpec wanted, obtained;
 
     /* Set the audio format */
@@ -183,10 +305,26 @@ bool initaudio(size_t nbsamples) {
     if (wanted.samples != obtained.samples) 
 	cerr << "obtained samples : "<< obtained.samples << "instead of : " << wanted.samples << endl;
     
+#ifdef CAROUBCANJACK
+    } // if (usejack) ... else ...
+#endif
     // this initialisation should probably move somewhere-else JDJDJDJD
     initSr ();
     initDphi ();
 
+    return true;
+}
+
+bool initaudio(size_t nbsamples) {
+    string appname("caroube-cli");
+    return initaudio (appname, nbsamples);
+}
+
+bool leaveaudio (void) {
+    pauseaudio (0);
+#ifdef CAROUBCANJACK
+    jack_client_close (jclient);
+#endif
     return true;
 }
 
